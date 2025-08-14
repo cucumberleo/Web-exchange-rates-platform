e53eaf485b45973a5e7a9bab31b422f78cbada0f
cucumberleo <3312381071@qq.com>
2025-08-14 11:37:40 +0800

    feat: add exit server logging

--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
 	"exchangeapp/config"
 	"exchangeapp/router"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
@@ -12,5 +18,25 @@ func main() {
 	if port == "" {
 		port = ":8080"
 	}
-	r.Run(port)
+	srv := &http.Server{
+		Addr: port,
+		Handler: r,
+	}
+	// add gracefully shut down server
+	go func ()  {
+		if err := srv.ListenAndServe(); err != nil && err!=http.ErrServerClosed{
+			log.Fatalf("Listen: %s\n",err)
+		}
+	}()
+	quit := make(chan os.Signal,1)
+	signal.Notify(quit,os.Interrupt)
+	<-quit
+	log.Println("Shutdown Sever ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(),5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil{
+		log.Fatal("Server Shutdown: ",err)
+	}
+	log.Println("Server exiting")
 }
